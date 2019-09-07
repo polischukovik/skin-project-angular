a50f31971ae84ceedb54e9fa0652d8161bf553a6
polischukovik <polischuk.kiev@gmail.com>
2019-09-08 00:50:09 +0300

    *

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html'
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
 
