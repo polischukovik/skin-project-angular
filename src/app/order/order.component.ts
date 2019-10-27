90de447d608d1e11cc0b80f902fc284cc3283f45
polischukovik <polischuk.kiev@gmail.com>
2019-10-27 20:31:25 +0200

    dialog

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { NovaPoshtaService } from './novaPoshta.service';
-import { City } from '../products/entity/city';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-order',
@@ -15,9 +15,16 @@ export class OrderComponent implements OnInit {
   private getId = (item: NpItem) => item.Ref;
   private getValue = (item: NpItem) => item.Description;
 
-  constructor(private npService: NovaPoshtaService) {}
+  constructor(private npService: NovaPoshtaService, public dialog: MatDialog) {}
 
   ngOnInit() { }
+
+  confirm() {
+    let dialogRef = this.dialog.open(UserProfileComponent, {
+      height: '400px',
+      width: '600px',
+    });
+  }
 
   onCitySelected(cityRef: string) {
     this.selectedCityRef = cityRef;
@@ -28,4 +35,25 @@ export class OrderComponent implements OnInit {
 interface NpItem {
   Ref: string;
   Description: string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+export interface DialogData {
+  animal: string;
+  name: string;
 }
